85673f2577f1b8e231c808a7b2e5bc7356637f1d
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-07-31 12:41:51 +0200

    Test with latest-plugins

--- runTests.sh
+++ runTests.sh
@@ -13,7 +13,7 @@ pushd test-project
 docker run -v //var/run/docker.sock:/var/run/docker.sock -v $(pwd):/workspace -v /tmp \
  -e BRANCH_NAME=master \
  -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e HOST=$(hostname) \
- ppiper/jenkinsfile-runner
+ ppiper/jenkinsfile-runner:latest-plugins
 
 popd
 
