7b1d008ed05c7e3a073fc74846c3f0d2a3adf071
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-08-27 16:33:48 +0200

    Build images for node 10, 12 and 14 (#17)
    Co-authored-by: Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
    Co-authored-by: Stephan Aßmus <stephan.assmus@sap.com>

--- runTests.sh
+++ runTests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 
-DOCKERFILE_PATH=${1:-.}
+TAG=${1:-.}
 
 # Start a local registry, to which we push the images built in this test, and from which they will be consumed in the test
 docker run -d -p 5000:5000 --restart always --name registry registry:2 || true
 
-docker build -t localhost:5000/ppiper/node-browsers:latest ${DOCKERFILE_PATH}
-docker tag localhost:5000/ppiper/node-browsers:latest ppiper/node-browsers:latest
+echo Testing version $TAG
+
+docker build --build-arg=BASE_IMAGE_TAG=${TAG} -t localhost:5000/ppiper/node-browsers:latest .
 docker push localhost:5000/ppiper/node-browsers:latest
 
 git clone https://github.com/piper-validation/cloud-s4-sdk-book.git -b validate-node-browsers test-project
