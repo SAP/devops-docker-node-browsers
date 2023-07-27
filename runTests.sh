750444303962f91c7fd2d0e6ef4977bd054bd72e
Oliver Feldmann <oliver.feldmann@sap.com>
2023-07-27 14:25:19 +0200

    Set new java version from build argument

--- runTests.sh
+++ runTests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -ex
 
 TAG=${1:-.}
+JAVA_VERSION=${2}
 
 echo Testing version $TAG
 
-docker build --build-arg=BASE_IMAGE_TAG=${TAG} -t localhost:5000/ppiper/node-browsers:${TAG} .
+docker build --build-arg=BASE_IMAGE_TAG=${TAG} --build-arg=JAVA_VERSION=${JAVA_VERSION} -t localhost:5000/ppiper/node-browsers:${TAG} .
 
 docker run --rm localhost:5000/ppiper/node-browsers:${TAG} chromium --version
 docker run --rm localhost:5000/ppiper/node-browsers:${TAG} firefox --version
